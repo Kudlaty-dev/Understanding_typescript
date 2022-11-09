d6fa76125038041a55e36cdfbb1b3231ac51be2d
Piotr Kowalski <perek.kowalski@gmail.com>
2022-11-09 16:35:28 +0100

    Basic TypeScript datatypes

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Piotr',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: 'admin'
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Piotr',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+//person.role.push('admin');
+//person.role[0] = 10
+var favoriteAvtivities;
+favoriteAvtivities = ['Sports'];
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is:';
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
--- app.ts
+++ app.ts
@@ -1,16 +1,40 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    
-    const result = n1 + n2
-    if(showResult) {
-        console.log(phrase + result)
-    } else{
-    return n1 + n2;
-    }
+
+
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: 'Piotr',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: 'admin'
+// }
+
+enum Role {ADMIN, AUTHOR, READ_ONLY};
+
+const person = {
+    name: 'Piotr',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is:'
+//person.role.push('admin');
+//person.role[0] = 10
 
-add(number1, number2, printResult, resultPhrase)
+
+let favoriteAvtivities: string[];
+favoriteAvtivities = ['Sports']
+
+console.log(person)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
+
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin')
+}
